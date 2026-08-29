1f5fec48be10cc15d104f0c68ae9fb76b22dc5f5
Tejash Prakash <tejashprakaram@gmail.com>
2026-08-29 13:11:31 +0530

    refactor: Remove deprecated chat window, message widget, and browser engine components

--- apps/browser/services/browser_engine.py
+++
@@ -1,8 +0,0 @@
-class BrowserEngine:
-    """Browser engine placeholder - replaced by chromium_engine.py"""
-    def __init__(self):
-        self.current_url = None
-
-    def load(self, url):
-        self.current_url = url
-        print(f"[BrowserEngine] Use chromium_engine instead")
